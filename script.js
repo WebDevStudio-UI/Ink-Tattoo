d007d6fda42b78a3204549c38085d082eac66cd7
WebDevStudio-UI <turatipropierties@gmail.com>
2026-03-31 23:38:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function inviaPrenotazione(event) {
+    event.preventDefault();
+    alert("Richiesta inviata con successo! Il nostro studio analizzerà la tua idea e ti contatterà al più presto per fissare un consulto.");
+    event.target.reset();
+}
